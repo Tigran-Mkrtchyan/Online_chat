a6d44cddd4cbbba8f5244c8e738f13ed4371395f
Tigran Mkrtchyan <Tigranmkrtchyan.07.06@gmail.com>
2020-07-07 14:21:24 +0400

    was done some changes

--- server/src/main/java/am/tech42/server/Server.java
+++ server/src/main/java/am/tech42/server/Server.java
@@ -13,7 +13,7 @@ public class Server {
 
     public static void main( String[] args ) {
          try(
-           ServerSocket server = new ServerSocket(80);
+            ServerSocket server = new ServerSocket(80);
             Socket socket =  server.accept();
         	BufferedReader in = new BufferedReader (new InputStreamReader(socket.getInputStream()));
         	BufferedWriter out = new BufferedWriter (new OutputStreamWriter( socket.getOutputStream()));
